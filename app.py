52191cb70f83c4ead1d926413575daa18aa4d357
Bia <beatriz.silva@agriness.com>
2020-11-06 14:31:06 -0300

    Adding new features

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def get_top_100(term: str, language: str = '') -> None:
         return None
     response = get_twitter_top_100({
         'q': term,
-        'result_type': 'popular',
         'count': 100,
         'lang': language
     })
@@ -29,25 +28,33 @@ def get_top_100(term: str, language: str = '') -> None:
         data = response.json()
         hashtags = []
         count = 0
+        continue_looking = True
         typer.echo('** Top 100 Tweets **\n')
-        while count < 100:
+        while data['search_metadata'].get('next_results') and continue_looking:
             for status in data['statuses']:
                 count += 1
                 typer.echo('Tweet {}: {}\n'.format(count, status['text']))
                 hashtags.extend(find_hashtag(status['text']))
-                if count == 100:
-                    break
+                tweet_url = 'https://twitter.com/{}/status/{}'.format(
+                    status['user']['screen_name'],
+                    status['id']
+                )
+                typer.echo(tweet_url)
+            hashtags_ranking = Counter(hashtags)
+            typer.echo('\n** Top Hashtags **\n')
+            count_hashtags = 0
+            for hashtag, ocurrences in hashtags_ranking.most_common(10):
+                count_hashtags += 1
+                typer.echo('{} - hashtag: #{}, ocurrences: {}'.
+                           format(count_hashtags, hashtag, ocurrences))
+
             next_page = data['search_metadata']['next_results']
             params_next_page = get_url_params(next_page)
-            get_twitter_top_100(params_next_page)
+            response = get_twitter_top_100(params_next_page)
             data = response.json()
-        hashtags_ranking = Counter(hashtags)
-        typer.echo('\n** Top Hashtags **\n')
-        count = 0
-        for hashtag, ocurrences in hashtags_ranking.most_common(10):
-            count += 1
-            typer.echo('{} - hashtag: #{}, ocurrences: {}'
-                       .format(count, hashtag, ocurrences))
+            if count >= 100:
+                answer = input('Do you want the next 100 tweets? (y/n)')
+                continue_looking = answer == 'y'
     else:
         typer.echo('Api call failed. Reason:')
         typer.echo(response.json())
@@ -58,7 +65,8 @@ def find_hashtag(tweet: str) -> List[str]:
 
 
 def get_url_params(querystring: str) -> dict:
-    return parse.parse_qs(querystring)
+    return {key.replace('?', ''): value[0] for key, value in
+            parse.parse_qs(querystring).items()}
 
 
 def get_twitter_top_100(params: dict) -> Response:
